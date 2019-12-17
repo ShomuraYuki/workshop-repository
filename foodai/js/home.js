10641d6b719ccc2c1a65791c6d9880fd8aaacd57
Yumi0501 <t19534yt@sfc.keio.ac.jp>
2019-12-18 07:23:17 +0900

    html,cssのデザインを完成

---
+++ foodai/js/home.js
@@ -0,0 +1,17 @@
+function sippo1(){
+var sippo1 = document.getElementById('sippo1');
+sippo1.classList.add('active');
+
+}
+
+function sippo2(){
+var sippo2 = document.getElementById('sippo2');
+sippo2.classList.add('active');
+
+}
+
+function sippo3(){
+var sippo3 = document.getElementById('sippo3');
+sippo3.classList.add('active');
+
+}
